bffa8c3fe8a6b62b5ff6f872e0bd29be000e325e
Vignesh Reddy <vigneshreddy@Vigneshs-MacBook-Air.local>
2023-10-01 16:20:33 -0400

    ass 1

--- subway.js
+++ subway.js
@@ -1,69 +1,84 @@
 const Order = require("./assignment1Order");
 
 const OrderState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    CHSUB: Symbol('Chichken Sub Size'),
-    MESUB: Symbol('MEAt SUB SIZE'),
-    CORTEA:   Symbol("COFFEEORTEA"),
-    TOPPINGS:   Symbol("toppings"),
-    DRINKS:  Symbol("drinks")
+    WELCOMING: Symbol("welcoming"),
+    DISH: Symbol("dish"),
+    SIZE: Symbol("size"),
+    TOPPINGS: Symbol("toppings"),
+    ICECREAM: Symbol("icecream"),
+    DRINKS: Symbol("drinks"),
 });
 
-module.exports = class SubwayOrder extends Order{
-    constructor(){
+module.exports = class SubwayOrder extends Order {
+    constructor() {
         super();
         this.stateCur = OrderState.WELCOMING;
+        this.selectedItem = "";
         this.sSize = "";
         this.sToppings = "";
         this.sDrinks = "";
-        this.orderItems = []
+        this.orderItems = [];
+        this.sItems = ["Chicken Sub", "Meat Sub"];
+        this.sIceCream = "";
     }
-    handleInput(sInput){
+
+    handleInput(sInput) {
         let aReturn = [];
-        switch(this.stateCur){
+        switch (this.stateCur) {
             case OrderState.WELCOMING:
-                this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcom to Vig SUBS");
-                aReturn.push("What kind of subs do you like ?, our chicken sub is $5 and meat sub $6");
+                this.stateCur = OrderState.DISH;
+                aReturn.push("Welcome to Vig SUBS");
+                aReturn.push("What kind of subs do you like?");
+                aReturn.push(...this.sItems);
+                break;
+            case OrderState.DISH:
+                if (sInput.toLowerCase() === "chicken sub") {
+                    this.selectedItem = sInput;
+                    this.orderItems.push(this.selectedItem);
+                    this.stateCur = OrderState.SIZE;
+                    aReturn.push(`What size of ${this.selectedItem} would you like?`);
+                    aReturn.push("Small ($5.99), Medium ($7.99), Large ($9.99)");
+                } else if (sInput.toLowerCase() === "meat sub") {
+                    this.selectedItem = sInput;
+                    this.orderItems.push(this.selectedItem);
+                    this.stateCur = OrderState.SIZE;
+                    aReturn.push(`What size of ${this.selectedItem} would you like?`);
+                    aReturn.push("Small ($6.99), Medium ($8.99), Large ($10.99)");
+                } else {
+                    aReturn.push("Invalid dish! Please choose Chicken Sub or Meat Sub.");
+                }
                 break;
             case OrderState.SIZE:
-                if(sInput.toLowerCase() == 'c'){
-                    aReturn.push('What size of chicken sub');
-                    this.orderItems.push('Chicken sub');
-                    this.stateCur = OrderState.CORTEA;
-                    break;
-                }else{
-                    aReturn.push('What size of meat sub');
-                    this.orderItems.push('meat sub');
-                    this.stateCur = OrderState.CORTEA;
-                    break;
+                this.sSize = sInput;
+                if (this.sSize.toLowerCase() === "small" || this.sSize.toLowerCase() === "medium" || this.sSize.toLowerCase() === "large") {
+                    this.stateCur = OrderState.TOPPINGS;
+                    aReturn.push("What toppings would you like?");
+                    this.orderItems.push(this.sSize);
+                } else {
+                    aReturn.push("Invalid size. Please enter Small, Medium, or Large.");
                 }
-            case OrderState.CORTEA:
-                this.orderItems.push(sInput);
-                aReturn.push('What toppings you would like with it');
-                this.stateCur = OrderState.CHSUB;
                 break;
-            case OrderState.CHSUB:
-                this.orderItems.push(sInput);
+            case OrderState.TOPPINGS:
+                this.sToppings = sInput;
+                this.stateCur = OrderState.ICECREAM;
+                aReturn.push("Would you like to add ice cream with your order?");
+                this.orderItems.push(this.sToppings);
+                break;
+            case OrderState.ICECREAM:
+                this.sIceCream = sInput;
                 this.stateCur = OrderState.DRINKS;
-                aReturn.push("Would you like to have iced tea with it");
+                aReturn.push("Would you like to add drinks with your order?");
+                this.orderItems.push(this.sIceCream);
                 break;
-            // case OrderState.MESUB:
-            //     this.orderItems.push(sInput);
-            //     this.stateCur = OrderState.DRINKS;
-            //     aReturn.push('Would you like to have iced tea with it ?')
-            //     break;
             case OrderState.DRINKS:
+                this.sDrinks = sInput;
+                this.orderItems.push(this.sDrinks);
                 this.isDone(true);
-                if(sInput.toLowerCase() != "no"){
-                    this.orderItems.push('with iced tea')
-                }
-                aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.orderItems[0]} ${this.orderItems[1]} with ${this.orderItems[2]}`);
-                if(this.orderItems[3]){
-                    aReturn.push('with iced tea');
-                }
-                let d = new Date(); 
+                let totalPrice = this.Price();
+                this.orderItems.push(`Total Price: $${totalPrice.toFixed(2)}`);
+                aReturn.push("Thank you for your order!");
+                aReturn.push(`You ordered: ${this.orderItems.join(", ")}`);
+                let d = new Date();
                 d.setMinutes(d.getMinutes() + 20);
                 aReturn.push(`Please pick it up at ${d.toTimeString()}`);
                 break;
@@ -71,4 +86,23 @@ module.exports = class SubwayOrder extends Order{
         return aReturn;
     }
 
-}
+    Price() {
+        const itemPrices = {
+            "chicken sub": {
+                small: 5.99,
+                medium: 7.99,
+                large: 9.99,
+            },
+            "meat sub": {
+                small: 6.99,
+                medium: 8.99,
+                large: 10.99,
+            },
+        };
+
+        const selectedItemPrice = itemPrices[this.selectedItem.toLowerCase()]?.[this.sSize.toLowerCase()] || 0;
+        let totalPrice = selectedItemPrice;
+
+        return totalPrice;
+    }
+};
